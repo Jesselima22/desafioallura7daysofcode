d63b653478513840087d217c77fbac1b395ef269
Jessé <jsslima22@hotmail.com>
2025-02-26 20:44:47 -0300

    Adicionando as resoluções dos desafios da Rafa Ballerini

---
+++ desafio2.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+console.log(`Olá${nome}, você tem ${idade} anos, e está aprendendo ${linguagem} que legal!, continue estudando!`);
